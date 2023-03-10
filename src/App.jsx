e0e62a9817061498a877db23aa0cfc5a581806d0
TorresGlz <alda.jzzg@gmail.com>
2023-03-09 20:45:58 -0600

    buttons and icons tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@
 //import Button from "@mui/material/Button"; //Permite que los componentes cargen mas rapido si los importas de manera literal
 
 import { Button, Container, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import AgricultureIcon from "@mui/icons-material/Agriculture";
+import AirIcon from '@mui/icons-material/Air';
 
 export default function App() {
   return (
     <Container>
       <h1>App</h1>
-      <Typography variant = "h3" color="primary">H3 titulo</Typography>
-      <Button variant ="contained">Boton</Button>
+      <Typography variant="h3" color="primary">
+        H3 titulo
+      </Typography>
+      <Button variant="outlined" color="error" startIcon={<AgricultureIcon/>}>
+        Boton
+      </Button>
+      <Button variant="contained" color="success" endIcon={<AirIcon/>}>
+        Boton
+      </Button>
     </Container>
   );
 }
